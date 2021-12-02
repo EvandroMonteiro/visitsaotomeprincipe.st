24cf759dbd9b8116f7def624860a68304421e064
Evandro Monteiro <goodvandro@gmail.com>
2021-12-02 12:25:52 +0000

    first commit

---
+++ nodejs/frontend/components/Downloads.vue
@@ -0,0 +1,25 @@
+<template>
+<section>
+  <div class="container">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-10">
+        <Card v-for="(item,indexDownloads) in items" :key="item.titulo+indexDownloads"
+          :titulo="item.nome"
+          :download="$t('cta.download.titulo')"
+          :link="'https://www.admin.saotome.made2grow.com'+item.ficheiro.url"
+          />
+    </div>
+  </div>
+</section>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+@Component({
+  name: 'ContentText'
+})
+export default class ContentText extends Vue {
+  @Prop() readonly items!: []
+  @Prop() readonly tipo!: string
+}
+</script>
